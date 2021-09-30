6056110122fbffe5b182310bd82416304b6ec1f8
tenceremitavami <fnurakdogan@gmail.com>
2021-09-30 22:56:20 +0300

    .

--- list_flat/flatten.py
+++ list_flat/flatten.py
@@ -1,11 +1,13 @@
-nests = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+inp = [[1, 'a', ['cat'], 2], [[[3]], 'dog'], 4, 5]
 
-def flatten(container):
-    for i in container:
-        if isinstance(i, (list,tuple)):
+
+def flatten(li):
+    for i in li:
+        if isinstance(i, (list, tuple)):
             for j in flatten(i):
                 yield j
         else:
             yield i
 
-print(list(flatten(nests)))
+
+print(list(flatten(inp)))
