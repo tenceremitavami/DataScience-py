b9b743dd412619d829d373c06287bea9632750c0
tenceremitavami <fnurakdogan@gmail.com>
2021-10-17 23:19:48 +0300

    -

---
+++ bootcamp-hw1/q1.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data = [['tom', 10], ['nick', 15], ['juli', 14]]
+df = pd.DataFrame(data, columns = ['Name', 'Age'])
---
+++ bootcamp-hw1/q2.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+
+data_set = pd.read_csv("data.csv")
